70459cdfd5bd64bfc7b51b7b52980930ae2c5265
sit3kk <sito.270@interia.pl>
2024-03-02 17:51:27 +0100

    Config

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: false,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()]
+});
