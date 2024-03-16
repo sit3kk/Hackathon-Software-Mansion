4cdc906e210fc4b955f3463164e7c3245933e281
sit3kk <sito.270@interia.pl>
2024-03-16 12:40:13 +0100

    Added api's

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
      path('accounts/', include('accounts.urls')),
+     path('events/', include('events.urls')),
+     path('socials/', include('socials.urls')),
+     
 ]
