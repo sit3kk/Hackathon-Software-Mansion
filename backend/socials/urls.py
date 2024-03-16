e9772743d3244d08e06230078927e4bd29b65f05
sit3kk <sito.270@interia.pl>
2024-03-16 13:33:56 +0100

    Socials features

--- backend/socials/urls.py
+++ backend/socials/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import FollowUserView, UnfollowUserView
+from .views import FollowUserView, UnfollowUserView, GetFollowingView, GetFollowersView, GetFollowingEventsView
 
 urlpatterns = [
     path('follow/<str:username>/', FollowUserView.as_view(), name='follow_user'),
     path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow_user'),
+    path('get_following/', GetFollowingView.as_view(), name='get_following'),
+    path('get_followers/', GetFollowersView.as_view(), name='get_followers'),
+    path('get_following_events/', GetFollowingEventsView.as_view(), name='get_following_events'),
+    
 ]
--- backend/socials/views.py
+++ backend/socials/views.py
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
+from events.models import Event
+
 class FollowUserView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -28,3 +30,34 @@ class UnfollowUserView(APIView):
             follow_relationship.delete()
             return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
         return Response({'error': 'Follow relationship does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetFollowingView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        following_users = Follow.objects.filter(follower=request.user)
+        following_usernames = [follow.followed.username for follow in following_users]
+        return Response({'following_users': following_usernames}, status=status.HTTP_200_OK)
+
+
+class GetFollowersView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        followers = Follow.objects.filter(followed=request.user)
+        follower_usernames = [follow.follower.username for follow in followers]
+        return Response({'followers': follower_usernames}, status=status.HTTP_200_OK)
+
+
+class GetFollowingEventsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        following_users = Follow.objects.filter(follower=request.user)
+        following_usernames = [follow.followed.username for follow in following_users]
+
+        following_events = Event.objects.filter(creator__username__in=following_usernames)
+
+
+        return Response({'following_events': following_events}, status=status.HTTP_200_OK)
